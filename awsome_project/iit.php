5e3c69f47112515f81edcffd63cdc02b3560ea9d
NikuuSamma <nickgarcialara@gmail.com>
2017-11-29 22:31:36 -0600

    Final upload

--- awsome_project/iit.php
+++ awsome_project/iit.php
@@ -4,11 +4,8 @@
     <meta charset='UTF-8'>
     <title>Illinois Institute of Technology</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' media='screen' href='css/reset.css'/>
-    <link rel='stylesheet' media='screen' href='css/normalize.css'/>
     <link rel='stylesheet' media='screen' href='css/global.css' />
     <link rel='stylesheet' media='screen' href='css/iit.css' />
-    <script src='js/iit.js'></script> 
   </head>
   
   <body>
@@ -25,7 +22,7 @@
     </div>
     <div id='main'>
       <div id='iitBanner'>
-         <span id='menu' onclick='openNav()'>&#9776;</span>
+         <span id='menu'>&#9776;</span>
         <div id='iitBannerBG'>
           <img src='img/header_blk_red.gif' alt='IIT banner'>
         </div>
@@ -49,12 +46,12 @@
             <img src="img/Academics_AcademicProfile.png" alt="AcademicProfile">
           </div>
           <div id="regrsitration">
-            <h1>Regrsitration Tools</h1>
+            <h1>Registration Tools</h1>
 						<p><a href="img/RegistrationStatus.png">Registration Status</a></p>
 						<p><a href="img/LooUpClasses.png">Look Up Classes</a></p>
 						<p><a href="img/AddDropClasses.png">Add or Drop Classes</a></p>
 						<p><a href="img/ChangeClassOptions.png">Change Class Options</a></p>
-						<p><a href="img/WeekAtGlance.png">Week at a Glance</a></p>
+						<p><a href="img/WeekAtGlance.png">Schedule - Week at a Glance</a></p>
 					</div>
           <div class="banner">
 							<img src="img/Academics_Banner.png" alt="Academic_Banner">		
@@ -62,5 +59,6 @@
         </div>
       </div>
     </div>
+		<script src='js/iit.js'></script> 
   </body>
 </html>
--- awsome_project/js/iit.js
+++ awsome_project/js/iit.js
@@ -1,14 +1,39 @@
+
+document.getElementById("menu").addEventListener("click", function()
+{
+    var navSize = document.getElementById("sideNav").style.width;
+    if (navSize == '250px' || navSize == '100px') 
+		{
+        return closeNav();
+    }
+    return openNav();
+});
+
 function openNav() 
 {
-  document.getElementById("sideNav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  
+	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+				
+	var deviceWidth = window.innerWidth;
+	
+	switch(true)
+	{
+		case (deviceWidth < 500):
+			document.getElementById("sideNav").style.width = "100px";
+			document.getElementById("main").style.marginLeft = "100px";
+		break;
+	
+		default:
+			document.getElementById("sideNav").style.width = "250px";
+			document.getElementById("main").style.marginLeft = "250px";
+		break;
+	}
 }
 
 function closeNav() 
 {
   document.getElementById("sideNav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+	document.body.style.backgroundColor = "white";
 }
 
 function openPage(page) 
